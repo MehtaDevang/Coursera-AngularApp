2d51348d71ecd657f95aad3fed1f2cc95d04d248
Devang Mehta <32339538+MehtaDevang@users.noreply.github.com>
2020-11-03 11:29:54 +0530

    added functionality for getting menu for each category

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
   angular
     .module("MenuCategoriesApp", [])
     .controller("MenuCategoriesController", MenuCategoriesController)
-    .service("MenuCategoriesService", MenuCategoriesService);
+    .service("MenuCategoriesService", MenuCategoriesService)
+    .constant("ApiBasePath", "https://davids-restaurant.herokuapp.com");
 
   MenuCategoriesController.$inject = ["MenuCategoriesService"];
   function MenuCategoriesController(MenuCategoriesService) {
@@ -14,20 +15,45 @@
     promise
       .then(function (response) {
         menu.categories = response.data;
+        console.log(menu.categories);
       })
       .catch(function (error) {
         console.log(error);
       });
+
+    menu.logMenuItems = function (shortName) {
+      var promise = MenuCategoriesService.getMenuForCategory(shortName);
+      promise
+        .then(function (response) {
+          menu.items = response.data;
+          console.log(menu.items);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
   }
 
-  MenuCategoriesService.$inject = ["$http"];
-  function MenuCategoriesService($http) {
+  MenuCategoriesService.$inject = ["$http", "ApiBasePath"];
+  function MenuCategoriesService($http, ApiBasePath) {
     var service = this;
 
     service.getMenuCategories = function () {
       var response = $http({
-        url: "https://davids-restaurant.herokuapp.com/categories.json"
+        url: ApiBasePath + "/categories.json"
       });
+      return response;
+    };
+
+    service.getMenuForCategory = function (shortName) {
+      var response = $http({
+        method: "GET",
+        url: ApiBasePath + "/menu_items.json",
+        params: {
+          category: shortName
+        }
+      });
+
       return response;
     };
   }
